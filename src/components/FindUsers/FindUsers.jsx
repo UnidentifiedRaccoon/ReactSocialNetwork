1460d0bc286dce6eec0d03559c2a8af31875cbc7
UnidentifiedRaccoon <yura.posledov@yandex.ru>
2020-11-08 23:01:59 +0300

    Получение данных пользователей с сервера (использование сервер API)

--- src/components/FindUsers/FindUsers.jsx
+++ src/components/FindUsers/FindUsers.jsx
@@ -1,43 +1,21 @@
 import React from 'react';
 import UserItem from './UserItem/UserItem'
 import cs from './FindUsers.module.css'
+import * as axios from 'axios'
 
 const FindUsers = (props) => {
-    if (props.usersData.length === 0) {
-        props.setUsers([
-            {
-                id: 0,
-                nickName: 'Mouse',
-                gender: 'female',
-                location: {country: 'UK', town: 'Brighton'},
-                followed: true,
-                imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png',
-                status: 'You never find me!',
-            },
-            {
-                id: 1,
-                nickName: 'Boogeyman',
-                gender: 'non-gender',
-                location: {country: 'Moon', town: 'MoonSity'},
-                followed: false,
-                imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png',
-                status: 'You never find me!',
-            },
-            {
-                id: 2,
-                nickName: 'Someone',
-                gender: 'male',
-                location: {country: 'Russia', town: 'No-town'},
-                followed: true,
-                imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Anonymous_emblem.svg/1200px-Anonymous_emblem.svg.png',
-                status: 'You never find me!',
-            },
-        ])
+    let setUsers = () => {
+        if (props.usersData.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {(props.setUsers(response.data.items))})
+        }
     }
-    let usersListElements = props.usersData.map(user => <UserItem data={user} follow={props.follow} unfollow={props.unfollow}/>);
+    let usersListElements = props.usersData.map(user => <UserItem data={user} follow={props.follow}
+                                                                  unfollow={props.unfollow}/>);
     return (
         <section>
             <h2>Users</h2>
+            <button onClick={setUsers}>Set Users</button>
             <ul className={cs.usersList}>
                 {usersListElements}
             </ul>
--- src/components/FindUsers/UserItem/UserItem.jsx
+++ src/components/FindUsers/UserItem/UserItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import cs from './UserItem.module.css'
+import icon from '../../../img/anon.png'
 
 const UserItem = (props) => {
     let user = props.data
@@ -7,19 +8,19 @@ const UserItem = (props) => {
         <li>
             <article>
                 <div>
-                    <img className={cs.icon} alt='UserIcon' src={user.imgSrc}/>
+                    <img className={cs.icon} alt='UserIcon' src={user.photos.small != null ? user.photos.small : icon} />
                     { user.followed
                        ?<button onClick={() => {props.unfollow(user.id)}}>Unfollow</button>
                         : <button onClick={() => {props.follow(user.id)}}>Follow</button>}
                 </div>
                 <div>
                     <div>
-                        <b>{user.nickName}</b>
+                        <b>{user.name}</b>
                         <i>{user.status}</i>
                     </div>
                     <div>
-                        <span>{user.location.country}</span>
-                        <span>{user.location.town}</span>
+                        <span>{'user.location.country'}</span>
+                        <span>{'user.location.town'}</span>
                     </div>
                 </div>
             </article>
